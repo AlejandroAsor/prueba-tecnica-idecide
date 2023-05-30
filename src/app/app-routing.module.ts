7e5fcbc6740312e3e48f2ff06751654af8538fc2
Alejandro Asor Corrales Gomez <84219142+AlejandroAsor@users.noreply.github.com>
2023-05-30 16:47:38 -0500

    Fixed inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,17 +5,17 @@ import { UsuariosComponent } from './usuarios/usuarios.component';
 import { ProductosComponent } from './productos/productos.component';
 import { CategoriasComponent } from './categorias/categorias.component';
 import { BuscarComponent } from './buscar/buscar.component';
-import { InicioComponent } from './inicio/inicio.component'; // Importa el componente InicioComponent
+import { InicioComponent } from './inicio/inicio.component';
 import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'usuarios', component: UsuariosComponent, canActivate: [AuthGuard] },
   { path: 'productos', component: ProductosComponent, canActivate: [AuthGuard] },
   { path: 'categorias', component: CategoriasComponent, canActivate: [AuthGuard] },
   { path: 'buscar', component: BuscarComponent, canActivate: [AuthGuard] },
-  { path: 'inicio', component: InicioComponent, canActivate: [AuthGuard] } // Agrega la ruta para el componente InicioComponent
+  { path: '', component: InicioComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
